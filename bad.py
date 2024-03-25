cc1bf869fb9e025befb0433ffde04648a4772018
Nathon Fowlie <2831833+nathonfowlie@users.noreply.github.com>
2024-03-26 09:36:46 +1100

    feat: add isort script
    Added script used to execute isort and parse the results to a
    workflow command.

---
+++ bad.py
@@ -0,0 +1,5 @@
+from typing import Any
+import path
+import os
+
+echo "Hello World"
--- files/app/entrypoint.sh
+++ files/app/entrypoint.sh
@@ -1,3 +1,7 @@
 #!/bin/sh -l
 
-echo "::error file=action.yml,title=some title,line=1,::this is a comment"
+#echo "::error file=action.yml,title=some title,line=1,::this is a comment"
+
+pipx ensurepath
+whereis isort
+#python3 /app/isort.py
